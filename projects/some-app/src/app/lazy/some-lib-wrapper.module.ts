21a026eeebc27e58ef5b6c3e1da1106ccb5f9730
Ofir Shemesh <ofir_shemesh@bmc.com>
2020-02-05 13:36:57 +0200

    get the lib from node modules

--- projects/some-app/src/app/lazy/some-lib-wrapper.module.ts
+++ projects/some-app/src/app/lazy/some-lib-wrapper.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
-
-import { SomeLibModule } from '@my-org/some-lib';
+import {SomeLibModule} from '@my-org/some-lib';
 
 @NgModule({
   imports: [SomeLibModule]
